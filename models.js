2a7c8799e0e6238af1bec7d1153739183f1c7b58
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-06-12 00:46:27 +0200

    Included bcrypt

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require ('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -24,6 +25,14 @@ let movieSchema = mongoose.Schema({
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
 
+  userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
+  
   let directorSchema = mongoose.Schema({
     Name:{type:String},
     Bio:{type:String},
