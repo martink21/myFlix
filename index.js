12dd58c9a917ec9936a1e18285d2a20165692701
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-06-15 00:23:28 +0200

    Edited the readability

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const Directors = Models.Director;
 /* mongoose.connect('mongodb://localhost:27017/myFlixDB', 
 { useNewUrlParser: true, useUnifiedTopology: true }); */
 mongoose.connect( process.env.CONNECTION_URI, 
-  { useNewUrlParser: true, useUnifiedTopology: true });const bodyParser = require('body-parser');
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
