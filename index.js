aabad913207257c59f24582cfda25fb128320058
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-06-14 23:57:21 +0200

    Replaced URI with config variable

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const Directors = Models.Director;
 
 /* mongoose.connect('mongodb://localhost:27017/myFlixDB', 
 { useNewUrlParser: true, useUnifiedTopology: true }); */
-mongoose.connect('mongodb:mongodb+srv://martinka:<bastun2020>@myflixdb.zwy55.mongodb.net/myFlixDB?retryWrites=true&w=majority', 
-{ useNewUrlParser: true, useUnifiedTopology: true });
-const bodyParser = require('body-parser');
+mongoose.connect( process.env.CONNECTION_URI, 
+  { useNewUrlParser: true, useUnifiedTopology: true });const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
